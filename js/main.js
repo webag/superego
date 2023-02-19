d70d8d9ce1c8fd149bd1ade3db4e51ff18fe7853
webag <andreiduffy@gmail.com>
2023-02-19 12:26:02 +0300

    123

--- js/main.js
+++ js/main.js
@@ -50,7 +50,112 @@ document.addEventListener("DOMContentLoaded", function () {
 
 /* Particles Begin */
 document.addEventListener("DOMContentLoaded", function () {
-	particlesJS.load('stage__particles', '/js/particles.json');
+	particlesJS('stage__particles',
+		{
+			"particles": {
+				"number": {
+					"value": 10,
+					"density": {
+						"enable": false,
+						"value_area": 800
+					}
+				},
+				"color": {
+					"value": "#ffffff"
+				},
+				"shape": {
+					"type": "circle"
+				},
+				"opacity": {
+					"value": 0.7,
+					"random": false,
+					"anim": {
+						"enable": false,
+						"speed": 1,
+						"opacity_min": 0.1,
+						"sync": false
+					}
+				},
+				"size": {
+					"value": 6,
+					"random": true,
+					"anim": {
+						"enable": false,
+						"speed": 40,
+						"size_min": 0.1,
+						"sync": false
+					}
+				},
+				"line_linked": {
+					"enable": true,
+					"distance": 650,
+					"color": "#ffffff",
+					"opacity": 0.4,
+					"width": 1
+				},
+				"move": {
+					"enable": true,
+					"speed": 2,
+					"direction": "none",
+					"random": false,
+					"straight": false,
+					"out_mode": "bounce",
+					"attract": {
+						"enable": false,
+						"rotateX": 600,
+						"rotateY": 1200
+					}
+				}
+			},
+			"interactivity": {
+				"detect_on": "canvas",
+				"events": {
+					"onhover": {
+						"enable": false,
+						"mode": "grab"
+					},
+					"onclick": {
+						"enable": false,
+						"mode": "push"
+					},
+					"resize": true
+				},
+				"modes": {
+					"grab": {
+						"distance": 400,
+						"line_linked": {
+							"opacity": 1
+						}
+					},
+					"bubble": {
+						"distance": 400,
+						"size": 40,
+						"duration": 2,
+						"opacity": 8,
+						"speed": 3
+					},
+					"repulse": {
+						"distance": 200
+					},
+					"push": {
+						"particles_nb": 4
+					},
+					"remove": {
+						"particles_nb": 2
+					}
+				}
+			},
+			"retina_detect": true,
+			"config_demo": {
+				"hide_card": false,
+				"background_color": "#b61924",
+				"background_image": "",
+				"background_position": "50% 50%",
+				"background_repeat": "no-repeat",
+				"background_size": "cover"
+			}
+		}
+	);
 })
 /* Particles End */
 
@@ -63,7 +168,7 @@ const initParallax = () => {
 	const pageH = window.innerHeight;
 	const timesPerSecond = 60;
 	let wait = false;
-	
+
 	// const blink = area.querySelector('.blink');
 	let moveX;
 	let moveY;
